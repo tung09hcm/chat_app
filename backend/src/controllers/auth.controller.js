10b4bf6960135b90f82dbb72427dc4b0e1e9e2d0
tung <tung.bui2004lk@hcmut.edu.vn>
2025-06-18 11:52:07 +0700

    logout login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,14 @@
 import bcrypt from "bcryptjs";
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 import bycrypt from "bcryptjs";
-import { generateToken } from "../lib/utils";
+import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req,res) => {
     const {fullName, email, password} = req.body
     try {
+        if(!fullName || !email || !password){
+            return res.status(400).json({message: "All fields are required"});
+        }   
         if(password.length < 6 )
         {
             return res.status(400).json({message: "Password must be at least 6 characters"});
@@ -30,6 +33,7 @@ export const signup = async (req,res) => {
                 fullName: newUser.fullName,
                 email: newUser.email,
                 profilePic: newUser.profilePic,
+                message: "Create User successfully"
             });
 
         }
@@ -41,9 +45,37 @@ export const signup = async (req,res) => {
         res.status(500).json({message: "Internal Server Error"});
     }
 }
-export const login = (req,res) => {
-    res.send("login route");
+export const login = async (req,res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await User.findOne({email})
+        if(!user)
+        {
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+        const isPasswordCorrect = await bycrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+            message: "Valid credential"
+        })
+    } catch (error) {
+        console.log("Error in login controller: ", error.message );
+        res.status(500).json({message: "Internal Server Error"});
+    }
 }
 export const logout = (req,res) => {
-    res.send("logout route");
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        return res.status(200).json({message: "Log out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller: ", error.message);
+        return res.status(500).json({message: "Internal server error"});
+    }
 }
